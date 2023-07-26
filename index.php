36704b902aa8592a65493bdba4b2f4f1026f0155
kangatharan <140579164+kangatharan@users.noreply.github.com>
2023-07-26 09:54:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Login Page</title>	
+		
+	</head>
+	<body>		
+		
+			<h1>Cloud Login Demo</h1>
+		
+		<hr>
+		
+				<h2>Login Details</h1>
+                <form action="login.php" method="POST">
+                    <label>User Name</label>
+                    <input type="text" name="usr"><br><br>
+                    <label>Password</label>
+                    <input type="password" name="pwd"><br>
+                    <input type="submit" value="Login">
+                </form>
+			
+	</body>
+</html>
+
+
